4d82976e4bc33efda3a5c85e6229007fd5cb2d88
Stefano Valloncini <stefanovalloncini@protonmail.com>
2021-04-21 10:40:12 +0200

    update

--- Tamagotchi/src/Tamagotchi.java
+++ Tamagotchi/src/Tamagotchi.java
@@ -9,22 +9,24 @@ public class Tamagotchi
 	private static final int BAD_SAZ_VALUE = 90;
 	private static final int GOOD_AFF_VALUE = 30; 
 	
+	private String nome; 
 	private int sodd_affettiva; 
 	private int grado_sazieta; 
 	private boolean isAlive; 
 	private boolean isFelice; 
 	
-	public Tamagotchi(int sodd_affettiva, int grado_sazieta)
+	public Tamagotchi(int sodd_affettiva, int grado_sazieta, String nome)
 	{
 		this.sodd_affettiva = sodd_affettiva; 
 		this.grado_sazieta = grado_sazieta; 
+		this.nome = nome; 
 		this.isTamaAlive(); 
 	}
 	
 	public void isTamaAlive()
 	{
 		this.isAlive = (sodd_affettiva > MIN_SAZ_VALUE && grado_sazieta > MIN_AFF_VALUE)
-					    || grado_sazieta < MAX_SAZ_VALUE; 
+					    && grado_sazieta < MAX_SAZ_VALUE; 
 	}
 	
 	public void isTamaFelice()
@@ -34,15 +36,28 @@ public class Tamagotchi
 	
 	public void daiBiscotto(int n_biscotti)
 	{
-		
+		this.grado_sazieta = Math.min(100, (this.grado_sazieta * 10 / 100) * n_biscotti); 
+		this.sodd_affettiva = Math.max(0, this.sodd_affettiva - n_biscotti / 4); 
+		this.isTamaAlive();
 	}
 	
 	public void daiCarezze(int n_carezze)
 	{
-		
+		this.sodd_affettiva = Math.min(100, this.sodd_affettiva + n_carezze); 
+		this.grado_sazieta = Math.max(0, grado_sazieta - n_carezze / 2); 
+		this.isTamaAlive();
 	}
 
 	// GETTERS AND SETTERS
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 	public int getSodd_affettiva() {
 		return sodd_affettiva;
 	}
--- Tamagotchi/src/TamagotchiMain.java
+++ Tamagotchi/src/TamagotchiMain.java
@@ -2,17 +2,35 @@ import java.util.Random;
 
 public class TamagotchiMain 
 {
-	private static final int MIN_BISCOTTI = 1;
-	private static final int MIN_CAREZZE = 1;
-	private static final int MAX_CAREZZE = 20;
-	private static final int MAX_BISCOTTI = 20;
+	private static final String INSERISCI_IL_NOME_DEL_TAMAGOTCHI = "Inserisci il nome del tamagotchi: ";
+	private static final String TAMAGOTCHI_MORTE = "Tamagotchi è morto. RIP Tama.";
+	private static final String TAMAGOTCHI_È_INFELICE = "Tamagotchi è infelice";
+	private static final String GRADO_SAZIETÀ = "Grado Sazietà: ";
+	private static final String SODDISFAZIONE_AFFETTIVA = "Soddisfazione affettiva: ";
+	private static final String TAMAGOTCHI_È_FELICE = "Tamagotchi è felice";
 	private static final String INSERIRE_IL_GRADO_DI_SAZIETÀ_INIZIALE = "Inserire il grado di sazietà iniziale: ";
 	private static final String INSERIRE_IL_GRADO_DI_SODDISFAZIONE_AFFETTIVA_INIZIALE = "Inserire il grado di soddisfazione affettiva iniziale: ";
 	private static String MESSAGGIO_INIZIALE = "Benvenuto nell'applicazione Tamagotchi. Qui puoi gestire il tuo tamagotchi."; 
 	private static String MESSAGGIO_MENU = "0. Esci dal programma\n"
-										 + "1. Dai biscotti [1 - 20]\n"	
-										 + "2. Dai carezze [1 - 20]\n"; 
+										 + "1. Dai biscotti\n"	
+										 + "2. Dai carezze\n"
+										 + "La tua scelta: "; 
 	
+	private static final int GRADO_SAZIETA_MASSIMO_INIZIALE = 100;
+	private static final int GRADO_SAZIETA_MINIMO_INIZIALE = 0;
+	private static final int SODDISFAZIONE_AFFETTIVA_MASSIMA_INIZIALE = 100;
+	private static final int SODDISFAZIONE_AFFETTIVA_MINIMA_INIZIALE = 0;
+	
+	private static final int MIN_BISCOTTI = 1;
+	private static final int MIN_CAREZZE = 1;
+	private static final int MAX_CAREZZE = 20;
+	private static final int MAX_BISCOTTI = 20;
+	
+	private static final int CHIUDI_PROGRAMMA = 0; 
+	private static final int DAI_BISCOTTI = 1; 
+	private static final int DAI_CAREZZE = 2;
+	
+
 	public static void main(String[] args) 
 	{
 		mostraMenu();
@@ -26,49 +44,50 @@ public class TamagotchiMain
 		
 		do
 		{
-			scelta = InputDati.inputInteger(MESSAGGIO_MENU, 0, 2); 
+			scelta = InputDati.inputInteger(MESSAGGIO_MENU, 0, 2); // 0 -> scelta minima possibile | 2 -> scelta massima possibile
 			
 			switch(scelta)
 			{
-				case 1: 
+				case DAI_BISCOTTI: 
 					tamagotchi.daiBiscotto(random.nextInt(MAX_BISCOTTI) + MIN_BISCOTTI);
 					break; 
-				case 2: 
+				case DAI_CAREZZE: 
 					tamagotchi.daiCarezze(random.nextInt(MAX_CAREZZE) + MIN_CAREZZE);
 					break;
-				case 0: 
+				case CHIUDI_PROGRAMMA: 
 					System.out.println("Mancherai al tuo tamagotchi!");
 			}
 			
-			if(tamagotchi.isAlive())
+			if(tamagotchi.isAlive() && scelta != 0)
 			{
 				if(tamagotchi.isFelice())
 				{
-					System.out.println("Tamagotchi è felice");
-					System.out.println("Soddisfazione affettiva: " + tamagotchi.getSodd_affettiva());
-					System.out.println("Grado Sazietà: " + tamagotchi.getGrado_sazieta());
+					System.out.println(TAMAGOTCHI_È_FELICE);
+					System.out.println(SODDISFAZIONE_AFFETTIVA + tamagotchi.getSodd_affettiva());
+					System.out.println(GRADO_SAZIETÀ + tamagotchi.getGrado_sazieta());
 				}
 				else
 				{
-					System.out.println("Tamagotchi è infelice");
-					System.out.println("Soddisfazione affettiva: " + tamagotchi.getSodd_affettiva());
-					System.out.println("Grado Sazietà: " + tamagotchi.getGrado_sazieta());
+					System.out.println(TAMAGOTCHI_È_INFELICE);
+					System.out.println(SODDISFAZIONE_AFFETTIVA + tamagotchi.getSodd_affettiva());
+					System.out.println(GRADO_SAZIETÀ + tamagotchi.getGrado_sazieta());
 				}
 			}
-			else
+			if(!tamagotchi.isAlive() && scelta != 0)
 			{
-				System.out.println("Tamagotchi è morto. RIP Tama.");
+				System.out.println(TAMAGOTCHI_MORTE);
 			}
 		}
-		while(scelta != 0 || !tamagotchi.isAlive()); 
+		while(scelta != 0 && tamagotchi.isAlive()); 
 	}
 	
 	public static Tamagotchi creaTamagotchi()
 	{
-		int sodd_aff_ini = InputDati.inputInteger(INSERIRE_IL_GRADO_DI_SODDISFAZIONE_AFFETTIVA_INIZIALE, 0, 100); 
-		int grad_saz_ini = InputDati.inputInteger(INSERIRE_IL_GRADO_DI_SAZIETÀ_INIZIALE, 0, 100); 
+		String nome = InputDati.inputString(INSERISCI_IL_NOME_DEL_TAMAGOTCHI); 
+		int sodd_aff_ini = InputDati.inputInteger(INSERIRE_IL_GRADO_DI_SODDISFAZIONE_AFFETTIVA_INIZIALE, SODDISFAZIONE_AFFETTIVA_MINIMA_INIZIALE, SODDISFAZIONE_AFFETTIVA_MASSIMA_INIZIALE); 
+		int grad_saz_ini = InputDati.inputInteger(INSERIRE_IL_GRADO_DI_SAZIETÀ_INIZIALE, GRADO_SAZIETA_MINIMO_INIZIALE, GRADO_SAZIETA_MASSIMO_INIZIALE); 
 		
-		Tamagotchi tamagotchi = new Tamagotchi(sodd_aff_ini, grad_saz_ini); 
+		Tamagotchi tamagotchi = new Tamagotchi(sodd_aff_ini, grad_saz_ini, nome); 
 
 		return tamagotchi; 
 	}
